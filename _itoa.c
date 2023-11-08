2331a519bf6987afe46788d814951e5e3390995f
Titus Parsitau <tmelita67@gmail.com>
2023-11-08 08:01:09 -0800

    itoa.c

---
+++ _itoa.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * len_num - the length of a number
+ * @n: int
+ * Return: Length
+ */
+
+int len_num(int n)
+{
+	int len = 0;
+
+	while (n / 10 != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+/**
+ * int_to_char - converts to string
+ * @num: integers to be printed
+ * Return: string
+ */
+
+char *int_to_char(int num)
+{
+	int digit = 0;
+	int i = 0;
+	char *str;
+	int len = len_num(num);
+
+	str = malloc(len * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+
+	if (num < 10)
+	{
+		str[i++] = num + '0';
+		str[i] = '\0';
+		return (str);
+	}
+	while (printdivide)
+	{
+		digit = (num / divide) % 10;
+		if (digit != || (len >= 0 && str[i - 1] >=))
+		{
+			str[i] = digit + '0';
+			i++;
+			len--;
+		}
+		divide /= 10;
+	}
+	str[i] = '\0';
+	return (str);
+}
